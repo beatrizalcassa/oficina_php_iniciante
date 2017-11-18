cf5b5dcf124b313f6f2a168ceb5aeb3ff0d76ee6
beatriz.alcassa <beatriz.alcassa@hotmail.com>
2017-11-18 14:00:24 -0500

    Meu primeiro commit.

--- index.php
+++ index.php
@@ -9,7 +9,129 @@
                         
         <?php
 
-            // Coloque seu código aqui!
+            /*$cor = "amarelo"; // campo para comentário
+            $cor = "vermelho";
+            echo $cor;
+      
+            var_dump(array("maçã", "maracujá"));
+      
+            $nome = "Ana";
+            echo "Olá " . $nome . "!";
+      
+            $a = 3;
+            $b = 5;
+            $resultado = $a +$b;
+            $resultado = $a -$b;
+            echo $resultado;
+      
+            $a = 5;
+            $b = 2;
+            
+            if($a > $b) {
+              echo "a é maior que b";
+            } else if($a == $b) {
+              echo "a e b são iguais";
+            } else {
+              echo "b é maior que a";
+            }
+      
+            $a = 1;
+            $b = 3;
+            
+            if($a > $b) {
+              echo "a é maior do que b";
+            } else if($a < $b) {
+              echo "a é menor do que b";
+            } else if($a == $b) {
+              echo "a e b são iguais";
+            }
+      
+            $i = 1;
+            while($i < 10) {
+              $i = $i +1;
+              echo $i . "<br>";
+            }
+      
+            $i = 1;
+            while($i < 10) {
+              $i = $i + 1; 
+            if($i%2 == 0) {
+              echo "O valor de $i é par<br>";
+            } else {
+              echo "O valor de $i é impar<br>"; 
+            }
+            }
+            
+      
+            for ($i = 1; $i < 10; $i ++) {
+              echo "O valor de i é: $i<br>";
+            }
+            
+            $nome = "Ana";
+            switch ($nome) {
+              case "Ana":
+                echo"Olá Ana";
+                break;
+              
+              case "Pedro":
+                echo"Olá Pedro";
+                break;
+              
+              default:
+                echo "Não sei quem é.";
+                
+              
+            }
+      
+            function soma($a, $b) {
+              $resultado = $a + $b;
+              return $resultado;
+            }
+            
+            $numero1 = 10.2;
+            $numero2 = 6;
+      
+            $resultadoFuncao = soma($numero1, $numero2);
+            echo $resultadoFuncao;     
+      
+            $cores = ["vermelho", "azul"];
+            echo $cores[0];
+      
+            $pessoa = ["nome" => "Ana", "idade" => 23];
+            echo $pessoa["nome"];
+      
+            $numeros = [1, 2, 3];
+            array_push($numeros, 4);
+            echo $numeros[3];
+      
+            $frutas = array("maçã", "laranja", "morango");
+            foreach($frutas as $fruta) {
+              echo "A fruta é $fruta<br>";
+            }*/
+      
+            $comidas = array("macarrão", "arroz", "feijão");
+            foreach($comidas as $comida) {
+              echo "A comida é $comida<br>";
+            }
+      
+            array_push($comidas, "sopa de cebola");
+            foreach($comidas as $comida) {
+              echo "A comida é $comida<br>";
+            }
+            
+            array_pop($comidas);
+            foreach($comidas as $comida) {
+              echo "A comida é $comida<br>";
+            }
+              
+             
+        
+            
+          
+         
+      
+            
+            
 
         ?>
 
